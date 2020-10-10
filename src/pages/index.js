f13613a698cb8749018c71dfea16fdaa03ee205f
Gabriel J <60169516+ecrseer@users.noreply.github.com>
2020-10-10 20:47:59 -0300

    too big title

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,6 +57,7 @@ export default function Home() {
               onChange={({target:{value}})=> setTempo(
                 {segundo:value,minuto:tempo.minuto}) }          
             />
+            
           </a>
 
 
