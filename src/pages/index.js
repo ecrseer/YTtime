f141848e5cf559a240d2732f7908d31fee76be95
Gabriel J <60169516+ecrseer@users.noreply.github.com>
2020-10-10 19:11:30 -0300

    objetify minuto tempo textfield inside card next js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,7 @@ import { useEffect } from "react";
 
 export default function Home() {
   const [urlvideo, setUrlvideo] = useState('____');
-  const [minuto,setMinuto] = useState(0);
-  const [segundo, setSegundo] = useState(0);
+  const [tempo,setTempo] = useState({minuto:0,segundo:0});
   const [urlModif,setUrlModif] = useState('');
 
   useEffect(()=>{
@@ -16,18 +15,19 @@ export default function Home() {
       {
        setUrlvideo(cip);
         }
+        ).then(()=>{
+
+          let tmp=
+            parseInt(tempo.minuto)
+            +parseInt(tempo.segundo);
+          setUrlModif(''+urlvideo+'?t='+tmp);
+          
+        }
         )
 
-  },[urlvideo])
+  },[urlvideo,tempo])
  
-  const tempoConvertido = () =>{
-    let soma=parseInt(segundo,10)+parseInt(minuto,10);
-    return soma;
-    //setSegundo(minuto)
-  }
-  function temsd() { 
-    return '2';
-  }
+  
   
   
   return (
@@ -40,59 +40,26 @@ export default function Home() {
       <main className={styles.main}>
         
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          {urlModif}
         </h1>
-        <TextField
-         id="standard-basic" name="min"
-        label="Minutos" onChange={({target:{value}})=>setMinuto(value*60)}  />
-
-        <TextField
-          id="standard-basic"
-          label="Segundos" name="seg"
-          onChange={({target:{value}})=> setSegundo(value)}
-          
-        />
-        <TextField
-          id="filled-basic"
-          label="Filled"
-          variant="filled"
-          value={`${urlvideo}?t=${temsd}`}
-          style={{width:"100%"}}
-        />
-
-        <p className={styles.description}>
-          Get started by editingg
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
+         
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a  className={styles.card}>
+              <TextField
+            id="standard-basic" name="min"
+            label="Minutos" onChange={({target:{value}})=>setTempo({
+              segundo:tempo.segundo, minuto:value*60
+            })}  />
+
+            <TextField
+              id="standard-basic"
+              label="Segundos" name="seg"
+              onChange={({target:{value}})=> setTempo(
+                {segundo:value,minuto:tempo.minuto}) }          
+            />
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
