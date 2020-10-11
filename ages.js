88b976ff27c11a68c3b56760a6b81bed80c2b883
Gabriel J <60169516+ecrseer@users.noreply.github.com>
2020-10-11 01:46:51 -0300

    ag

--- ages.js
+++ ages.js
@@ -1,18 +1,21 @@
 navigator.permissions.query({name:'clipboard-read',
-      allowWithoutGesture: false}).then(result=>
-           result.state=='granted'||'prompt'?
-           '':'')
-           navigator.
-           clipboard.readText().then((cip) => 
-             {
-              setUrlvideo(cip);
-               }
-               ).then(()=>{
-       
-                 let tmp=
-                   parseInt(tempo.minuto)
-                   +parseInt(tempo.segundo);
-                 setUrlModif(''+urlvideo+'?t='+tmp);
+      allowWithoutGesture: false}).then(result=>{
+
+           if(result.state=='granted'||
+                result.state=='prompt'){           
+                navigator.
+                clipboard.readText().then((cip) => 
+                  {
+                    setUrlvideo(cip);
+                  }  
+                    ).then(()=>{
+                        let tmp=
+                          parseInt(tempo.minuto)
+                           +parseInt(tempo.segundo);
+                              setUrlModif(''+urlvideo+'?t='+tmp);
                  
                }
+              }
+            }
                )
+      )
