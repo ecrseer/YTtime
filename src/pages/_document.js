7db608ee2248a2c763c6f4adbab5b9dd4840c412
Gabriel Justino <60169516+ecrseer@users.noreply.github.com>
2020-12-11 15:00:13 -0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,18 +1,22 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-  render() { 
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
     return (
       <Html>
-	<Head />
-	<body>
-	  <Main />
-	  <NextScript />
-        <script src="" />
-	</body>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
       </Html>
     )
   }
 }
 
-export default MyDocument;
+export default MyDocument
